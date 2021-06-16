5a6002c45c35f9baefb36c85761239b2915efd14
Rituraj Choudhury <85727631+API1246@users.noreply.github.com>
2021-06-16 15:48:38 +0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package pk1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+    System.out.println("hello from mahesh karad !");
+    
+    
+    System.out.println("hello from Rituraj!");
+	}
+}
